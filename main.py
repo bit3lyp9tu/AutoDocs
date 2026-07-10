dda54edcbad2fca37575167184a5e6b981292237
makl812c <makl812c@tu-dresden.de>
2026-07-10 13:57:43 +0200

    wip

--- main.py
+++ main.py
@@ -21,25 +21,27 @@ def main():
         "zai-org/GLM-5.2-FP8"
     ]
 
+    # source = "tests/example_data/oop.py"
+    # target_path = f"tests/results/benchmarks/uml_class_diagram/"
+    source = "tests/example_data/db_schema.py"
+    target_path = f"tests/results/benchmarks/entity_relation_diagram/"
+
     yaml = YAMLConfig('tests/configs/autodocs.yaml')
 
     for i in range(len(model_list)):
         print(f"Applying Model [{model_list[i]}]...")
 
-        # source = "tests/example_data/oop.py"
-        # target = f"tests/results/benchmarks/uml_class_diagram/test_{i}.puml"
-        source = "tests/example_data/db_schema.py"
-        target = f"tests/results/benchmarks/entity_relation_diagram/test_{i}.puml"
+        file_path = target_path + f"test_{i}.puml"
 
         PlantUMLConverter(
             config=yaml.config,
             model=model_list[i]
         ).convert(
             source_file=source,
-            target_path=target
+            target_path=file_path
         )
 
-        PlantUMLRendering(config=yaml.config, render_source_path=target).render()
+        PlantUMLRendering(config=yaml.config, render_source_path=file_path).render()
 
         sleep(yaml.config.llm_service.api.request_delay_seconds)
 
---
+++ src/git_master.py
@@ -0,0 +1,10 @@
+
+from src.config_model import Config
+
+
+class GitMaster:
+    def __init__(self, config: Config) -> None:
+        self.config = config
+
+    def diff(self):
+        pass
